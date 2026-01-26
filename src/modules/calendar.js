fe9005bbea308d407f2077af4e762e1ea3b28713
Johannes Backhaus <johannes@Mac.fritz.box>
2026-01-26 11:37:38 +0100

    style: enhance visibility of search and selection inputs, bump version to v1.2.0

--- src/modules/calendar.js
+++ src/modules/calendar.js
@@ -1,7 +1,7 @@
 import { state } from '../store.js';
 import { db, firebase } from '../config.js';
 
-const APP_VERSION = 'v1.1.0-search';
+const APP_VERSION = 'v1.2.0';
 
 export function initCalendar() {
     console.log("AppDate Version:", APP_VERSION);
