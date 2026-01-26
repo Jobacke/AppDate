c207709fcd44bac1254cd1d90d7bb8ca9bf722cf
Johannes Backhaus <johannes@Mac.fritz.box>
2026-01-26 11:44:21 +0100

    style: rename selection button to 'Löschauswahl', bump version to v1.2.2

--- src/modules/calendar.js
+++ src/modules/calendar.js
@@ -1,7 +1,7 @@
 import { state } from '../store.js';
 import { db, firebase } from '../config.js';
 
-const APP_VERSION = 'v1.2.1';
+const APP_VERSION = 'v1.2.2';
 
 export function initCalendar() {
     console.log("AppDate Version:", APP_VERSION);
