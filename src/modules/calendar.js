28cdcba11cd541b247229283018871966fc60dd7
Johannes Backhaus <johannes@Mac.fritz.box>
2026-01-26 22:25:43 +0100

    Stronger regex filter for curly braces

--- src/modules/calendar.js
+++ src/modules/calendar.js
@@ -989,7 +989,7 @@ function renderCalendar() {
                         </div>
                     </div>
                 </div>
-                ${(evt.description && evt.description.trim() !== '{') ? `<div class="mt-2 text-xs text-br-400 line-clamp-2">${evt.description}</div>` : ''}
+                ${(evt.description && !/^[\s\{\}]+$/.test(evt.description)) ? `<div class="mt-2 text-xs text-br-400 line-clamp-2">${evt.description}</div>` : ''}
             </div>`;
         });
         html += `</div></div>`;
