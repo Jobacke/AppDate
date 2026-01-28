3be93976929c563fefe9254b3e0f18c2dd540b94
Johannes Backhaus <johannes@Mac.fritz.box>
2026-01-28 23:18:07 +0100

    fix(version): update JS APP_VERSION constant to v1.3.1

--- src/modules/calendar.js
+++ src/modules/calendar.js
@@ -1,7 +1,7 @@
 import { state } from '../store.js';
 import { db, firebase } from '../config.js';
 
-const APP_VERSION = 'v1.2.6';
+const APP_VERSION = 'v1.3.1';
 
 export function initCalendar() {
     console.log("AppDate Version:", APP_VERSION);
