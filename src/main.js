e78331e98910b9ff46936d356cfea11ff2101cb2
Johannes Backhaus <johannes@Mac.fritz.box>
2026-01-29 20:18:21 +0100

    Fix: Force unregister potential stale service workers

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import { initCalendar } from './modules/calendar.js';
 import { initSecurity } from './modules/security.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Force unregister legacy service workers
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      for (let registration of registrations) {
+        registration.unregister();
+        console.log("Legacy Service Worker unregistered.");
+      }
+    });
+  }
+
   initSecurity(); // Initialize security first to lock screen if needed
   initAuth();
   initCalendar();
